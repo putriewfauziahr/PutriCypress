8b3d011f31c36c7678f327516fd7090d0fe7397f
putriewfauziahr <putrifauziahrahman@gmail.com>
2023-12-06 15:08:56 +0700

    Add testcases login using custom commands

--- cypress/support/E2E/login.js
+++ cypress/support/E2E/login.js
@@ -3,24 +3,32 @@ import "../commands";
 
 describe('Login', () => {
     it('Login', () => {
-
         cy.visit('/');
         cy.get('.home-main>.content>.title').should('contain', 'Get fit and look fab in new seasonal styles');
         cy.get('.panel>.header>.authorization-link>a').click();
-        cy.get('#email').type('putri.tester@mailinator.com');       
-        cy.get('#pass').type('@123Testing');
-        cy.get('#send2').click();
-        cy.wait(2000);
-        cy.xpath('//header/div[1]/div[1]/ul[1]/li[1]/span[1]').should('contain','Welcome,')
+        cy.get('#email').should('be.visible').type('putri.tester@mailinator.com');       
+        cy.get('#pass').should('be.visible').type('@123Testing');
         cy.wait(3000);
-        cy.xpath('//header/div[1]/div[1]/ul[1]/li[2]/span[1]/button[1]').click();
-        cy.wait(2000);
-        cy.get(':nth-child(2) > .customer-welcome > .customer-menu > .header > .authorization-link > a').click();
+        cy.get('#send2').click();
+        cy.get('.logged-in', {timeout:5000}).should('be.visible').should('contain','Welcome,');
+        cy.get('button.action.switch').should('be.visible').eq(0).click();
+        cy.get(':nth-child(2) > .customer-welcome > .customer-menu > .header > .authorization-link > a').should('be.visible').click();
         cy.get('.panel > .header > .authorization-link > a').should('contain', 'Sign In ');
-
+        cy.wait(3000);
     })
 
-    it.only('Login Using Custom Commands', () => {
+    it('Login Using Invalid Credential', () => {
         cy.loginWithInvalidCredentials();
+        cy.wait(3000);
+    })
+
+    it('Login Using Valid Credential', () => {
+        cy.loginWithValidCredentials();
+        cy.wait(3000);
+        
+    })
+
+    it.only('Login Without Input Credentail', () => {
+        cy.LoginWithoutInputCredential();
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
+Cypress.Commands.add('loginWithValidCredentials', () => {
+  cy.fixture('../fixtures/data.json').then((loginData) => {
+    const ValidCredential = Cypress._.sample(loginData.ValidCredential);
+    cy.visit('/');
+    cy.get('.home-main>.content>.title').should('contain', 'Get fit and look fab in new seasonal styles');
+    cy.get('.panel>.header>.authorization-link>a').click();
+    cy.get('#email').type(ValidCredential.email);
+    cy.get('#pass').type(ValidCredential.pass);
+    cy.get('#send2').should('be.visible').click();
+    cy.get('.logged-in', {timeout:5000}).should('be.visible').should('contain', ValidCredential['success-msg-login']);
+  });
+});
+
+
  Cypress.Commands.add('loginWithInvalidCredentials', () => {
     cy.fixture('../fixtures/data.json').then((loginData) => {
       const InvalidCredential = Cypress._.sample(loginData.InvalidCredential);
@@ -18,20 +20,21 @@
       cy.get('.panel>.header>.authorization-link>a').click();
       cy.get('#email').type(InvalidCredential.email);
       cy.get('#pass').type(InvalidCredential.pass);
+      cy.get('#send2').should('be.visible').click();
+      cy.get('.message-error>div').should('be.visible').should('contain', InvalidCredential['error-msg-login']);
+    });
+  });
+
+
+  Cypress.Commands.add('LoginWithoutInputCredential', () => {
+    cy.fixture('../fixtures/data.json').then((loginData) => {
+      const WithoutCredential = Cypress._.sample(loginData.ValidCredential);
+      cy.visit('/');
+      cy.get('.home-main>.content>.title').should('contain', 'Get fit and look fab in new seasonal styles');
+      cy.get('.panel>.header>.authorization-link>a').click();
+      cy.wait(3000);
       cy.get('#send2').click();
-      cy.wait(2000);
-      cy.xpath('//div[contains(text(),"The account sign-in was incorrect or your account")]').should('contain', 'The account sign-in was incorrect or your account');
-  });
-  });
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+      cy.get('#email-error').should('be.visible').should('contain', WithoutCredential['error-email']);
+      cy.get('#pass-error').should('be.visible').should('contain', WithoutCredential['error-pass']);
+    })
+  })
